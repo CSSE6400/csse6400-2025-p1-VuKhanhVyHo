ef22ce850a2b86007d6f4018302644d6a5b3bd02
Vu Khanh Vy Ho <116183364+VuKhanhVyHo@users.noreply.github.com>
2025-02-24 23:51:40 +0000

    Some message here

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask 
  
 def create_app(): 
-   app = Flask(__name__) 
+    app = Flask(__name__) 
  
-   from .views.routes import api 
-   app.register_blueprint(api) 
+    from .views.routes import api 
+    app.register_blueprint(api) 
  
-   return app
+    return app
